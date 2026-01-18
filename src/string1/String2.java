aa25943d43419ae24b6aaf546557309a2a068a9d
Ali <@.com>
2026-01-18 19:38:20 +0600

    countHi

--- src/string1/String2.java
+++ src/string1/String2.java
@@ -9,5 +9,13 @@ public class String2 {
         }
         return str1;
     }
-
+    public int countHi(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length()-1; i++) {
+            if (str.substring(i,i+2).equals("hi")){
+                count++;
+            }
+        }
+        return count;
+    }
 }
