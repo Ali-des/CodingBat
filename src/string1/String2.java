c99175344cf24544eee8d7d0e058c73a6a854467
Ali <@.com>
2026-01-18 19:39:36 +0600

    countCade

--- src/string1/String2.java
+++ src/string1/String2.java
@@ -35,4 +35,12 @@ public class String2 {
             return false;
         }
     }
+    public int countCode(String str) {
+        int count=0;
+        for (int i = 0; i < str.length()-3; i++) {
+            if (str.substring(i,i+4).matches("co.e")){
+                count++;
+            }
+        }return count;
+    }
 }
