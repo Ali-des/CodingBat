03f729623bc470ddf3c02fd1344e5240eed90ff4
Ali <@.com>
2026-01-18 19:38:50 +0600

    catDog

--- src/string1/String2.java
+++ src/string1/String2.java
@@ -18,4 +18,21 @@ public class String2 {
         }
         return count;
     }
+    public boolean catDog(String str) {
+        int c=0;
+        int d=0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i,i+3).equals("cat")){
+                c++;
+            }
+            if (str.substring(i,i+3).equals("dog")){
+                d++;
+            }
+        }
+        if (c==d){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
